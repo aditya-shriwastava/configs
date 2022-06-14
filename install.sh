8b5cf5807027e8083eadde9c9256885450bf4681
Aditya Shriwastava <adityashriwastava7@gmail.com>
2022-06-15 05:11:16 +0530

    Update

--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-apt-get --assume-yes update
+sudo apt-get --assume-yes update
 
-apt-get --assume-yes install xclip
+sudo apt-get --assume-yes install xclip
 echo 'cpfile(){
   readlink -f "$1" | xclip -selection clipboard
 }' >> ~/.bashrc
 
-apt-get --assume-yes install git
+sudo apt-get --assume-yes install git
 cp ./.gitconfig ~
 
 ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa.pub | xclip -selection clipboard
 
-apt-get --assume-yes install vim
+sudo apt-get --assume-yes install vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp ./.vimrc ~
@@ -22,20 +22,20 @@ vim +PlugInstall +qall
 echo 'set -o vi
 bind -m vi-insert "\C-l":clear-screen' >> ~/.bashrc
 
-apt-get --assume-yes install tmux
+sudo apt-get --assume-yes install tmux
 cp ./.tmux.conf ~
 
-apt-get --assume-yes install ranger
+sudo apt-get --assume-yes install ranger
 echo "source /usr/share/doc/fzf/examples/key-bindings.bash" >> ~/.bashrc
 echo "source /usr/share/doc/fzf/examples/completion.bash" >> ~/.bashrc
 
-apt-get --assume-yes install fzf
+sudo apt-get --assume-yes install fzf
 echo "export FZF_DEFAULT_COMMAND='find .'" >> ~/.bashrc
 echo 'export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"' >> ~/.bashrc
 
-apt-get --assume-yes install neofetch
-apt-get --assume-yes install htop
-apt-get --assume-yes install nmap
-apt-get --assume-yes install trash-cli
+sudo apt-get --assume-yes install neofetch
+sudo apt-get --assume-yes install htop
+sudo apt-get --assume-yes install nmap
+sudo apt-get --assume-yes install trash-cli
 
 source ~/.bashrc
