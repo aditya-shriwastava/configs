52ecffed1f44f0ba9c09b8c630c3f7f4d7887987
Aditya Shriwastava <adityashriwastava7@gmail.com>
2022-06-15 05:50:22 +0530

    Update

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "$(whoami)"
+user=$(whoami)
 [ "$UID" -eq 0 ] || exec sudo "$0" "$@"
 
 sudo apt-get --assume-yes update
@@ -8,11 +8,11 @@ sudo apt-get --assume-yes update
 sudo apt-get --assume-yes install xclip
 echo 'cpfile(){
   readlink -f "$1" | xclip -selection clipboard
-}' >> ~/.bashrc
+}' >> /home/$user/.bashrc
 
 sudo apt-get --assume-yes install git
 cp ./.gitconfig ~
-cat ./ps1.txt >> ~/.bashrc
+cat ./ps1.txt >> /home/$user/.bashrc
 
 ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
 
@@ -23,23 +23,22 @@ cp ./.vimrc ~
 vim +PlugInstall +qall
 
 echo 'set -o vi
-bind -m vi-insert "\C-l":clear-screen' >> ~/.bashrc
+bind -m vi-insert "\C-l":clear-screen' >> /home/$user/.bashrc
 
 sudo apt-get --assume-yes install tmux
 cp ./.tmux.conf ~
 
 sudo apt-get --assume-yes install ranger
-echo "source /usr/share/doc/fzf/examples/key-bindings.bash" >> ~/.bashrc
-echo "source /usr/share/doc/fzf/examples/completion.bash" >> ~/.bashrc
+echo "source /usr/share/doc/fzf/examples/key-bindings.bash" >> /home/$user/.bashrc
+echo "source /usr/share/doc/fzf/examples/completion.bash" >> /home/$user/.bashrc
 
 sudo apt-get --assume-yes install fzf
-echo "export FZF_DEFAULT_COMMAND='find .'" >> ~/.bashrc
-echo 'export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"' >> ~/.bashrc
+echo "export FZF_DEFAULT_COMMAND='find .'" >> /home/$user/.bashrc
+echo 'export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"' >> /home/$user/.bashrc
 
 sudo apt-get --assume-yes install neofetch
 sudo apt-get --assume-yes install htop
 sudo apt-get --assume-yes install nmap
 sudo apt-get --assume-yes install trash-cli
 
-source ~/.bashrc
-cat ~/.ssh/id_rsa.pub
+cat /home/$user/.ssh/id_rsa.pub
