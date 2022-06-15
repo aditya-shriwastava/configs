f29e5286880401acbe7661e6e2e6a44cfe411c92
Aditya Shriwastava <adityashriwastava7@gmail.com>
2022-06-15 06:01:41 +0530

    Update

--- install.sh
+++ install.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
-user=$(whoami)
-[ "$UID" -eq 0 ] || exec sudo "$0" "$@"
+if ! [ $(id -u) = 0 ]; then
+   echo "The script need to be run as root." >&2
+   exit 1
+fi
 
-sudo apt-get --assume-yes update
+if [ $SUDO_USER ]; then
+    user=$SUDO_USER
+else
+    user=$(whoami)
+fi
 
-sudo apt-get --assume-yes install xclip
+apt-get --assume-yes update
+
+apt-get --assume-yes install xclip
 echo 'cpfile(){
   readlink -f "$1" | xclip -selection clipboard
 }' >> /home/$user/.bashrc
 
-sudo apt-get --assume-yes install git
+apt-get --assume-yes install git
 cp ./.gitconfig ~
 cat ./ps1.txt >> /home/$user/.bashrc
 
 ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
 
-sudo apt-get --assume-yes install vim
+apt-get --assume-yes install vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp ./.vimrc ~
@@ -25,20 +33,20 @@ vim +PlugInstall +qall
 echo 'set -o vi
 bind -m vi-insert "\C-l":clear-screen' >> /home/$user/.bashrc
 
-sudo apt-get --assume-yes install tmux
+apt-get --assume-yes install tmux
 cp ./.tmux.conf ~
 
-sudo apt-get --assume-yes install ranger
+apt-get --assume-yes install ranger
 echo "source /usr/share/doc/fzf/examples/key-bindings.bash" >> /home/$user/.bashrc
 echo "source /usr/share/doc/fzf/examples/completion.bash" >> /home/$user/.bashrc
 
-sudo apt-get --assume-yes install fzf
+apt-get --assume-yes install fzf
 echo "export FZF_DEFAULT_COMMAND='find .'" >> /home/$user/.bashrc
 echo 'export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"' >> /home/$user/.bashrc
 
-sudo apt-get --assume-yes install neofetch
-sudo apt-get --assume-yes install htop
-sudo apt-get --assume-yes install nmap
-sudo apt-get --assume-yes install trash-cli
+apt-get --assume-yes install neofetch
+apt-get --assume-yes install htop
+apt-get --assume-yes install nmap
+apt-get --assume-yes install trash-cli
 
 cat /home/$user/.ssh/id_rsa.pub
